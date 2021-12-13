02c89d15d353073be004515699d8aed895044b7a
Jakob Wagner <jakob.wagner@methodpark.de>
2021-12-13 14:30:38 +0100

    more functional

--- 2021/p13.py
+++ 2021/p13.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-from typing import List, Tuple
+from typing import List, Tuple, TypeVar
 from aocd import lines, submit
-from operator import and_, countOf
+from operator import add
 from functools import reduce
-
-# from copy import deepcopy
 
 Map = List[List[int]]
 Fold = Tuple[str, int]
+T = TypeVar('T')
 
 
-def elementwise(func, iterable1, iterable2):
+def combine_lists(func, iterable1, iterable2):
     return list(map(func, iterable1, iterable2))
 
 
@@ -23,21 +22,17 @@ def create_map(dimensions: Tuple[int, int]) -> Map:
 
 
 def parse(lines: List[str]) -> Tuple[Map, List[Fold]]:
-    dimensions: Tuple[int, int] = (0, 0)
-    points: List[Tuple[int, int]] = []
+    point_lines = lines[:lines.index("")]
+    points = [tuple(map(int, reversed(line.split(','))))
+              for line in point_lines]
 
-    line_iterator = iter(lines)
-    for line in line_iterator:
-        line = line.strip()
-        if not line:
-            break
-        x, y = list(map(int, line.split(',')))
-        points.append((y, x))
-        dimensions = elementwise(max, dimensions, (y + 1, x + 1))
+    dimensions = reduce(lambda acc, x: map(
+        max, acc, x), points, (0, 0))
+    dimensions = tuple(map(add, dimensions, (1, 1)))
 
-    folds: List[Fold] = []
-    for line in line_iterator:
-        folds.append((line[11], int(line[13:])))
+    fold_lines = lines[lines.index("") + 1:]
+    folds = [((line[11], int(line[13:])))
+             for line in fold_lines]
 
     paper = create_map(dimensions)
     for point in points:
@@ -45,40 +40,41 @@ def parse(lines: List[str]) -> Tuple[Map, List[Fold]]:
     return (paper, folds)
 
 
+def split_at(list: List[T], index: int) -> Tuple[List[T], List[T]]:
+    return list[:index], list[index + 1:]
+
+
 def fold(paper: Map, fold: Fold) -> Map:
     # fold up
     if fold[0] == 'y':
-        top, bottom = paper[:fold[1]], paper[fold[1] + 1:]
-        if len(top) > len(bottom):
-            bottom += create_map((len(top) - len(bottom), len(top[0])))
-        elif len(top) < len(bottom):
-            top = create_map((len(top) - len(bottom), len(top[0]))) + top
-        assert(len(top) == len(bottom))
-        paper = list(
-            map(lambda x, y: elementwise(lambda x, y: x or y, x, y), top, reversed(bottom)))
+        top, bottom = split_at(paper, fold[1])
+        # pad halves in case the fold is not centered
+        bottom += create_map((max(0, len(top) - len(bottom)), len(top[0])))
+        top = create_map(
+            (max(0, len(top) - len(bottom)), len(top[0]))) + top
+        return [combine_lists(lambda x, y: x or y, x, y)
+                for x, y in zip(top, reversed(bottom))]
+    # fold left
     else:
-        new = []
-        for row in paper:
-            left, right = row[:fold[1]], row[fold[1] + 1:]
-            assert(len(left) == len(right))
-            new_row = list(
-                map(lambda x, y: x or y, left, reversed(right)))
-            new.append(new_row)
-        paper = new
-    return paper
+        # no let expression in python, so we need to define a local function
+        def folder(row):
+            left, right = split_at(row, fold[1])
+            return [x or y for x, y in zip(left, reversed(right))]
+        return [folder(row) for row in paper]
 
 
 def part1(paper: Map, folds: List[Fold]) -> int:
     paper = fold(paper, folds[0])
-    num_dots = reduce(lambda total, row: total + sum(row), paper, 0)
+    num_dots = sum(map(lambda row: sum(row), paper))
     return num_dots
 
 
-def part2(paper: Map, folds: List[Fold]) -> int:
-    for folding in folds:
-        paper = fold(paper, folding)
-    [print(row) for row in paper]
-    return 0
+def part2(paper: Map, folds: List[Fold]) -> Map:
+    if folds:
+        head, *tail = folds
+        return part2(fold(paper, head), tail)
+    else:
+        return paper
 
 
 def main():
@@ -119,7 +115,23 @@ def main():
     # part2(example_paper, example_folds)
     print(f"b")
     answer_b = part2(paper, folds)
-    # assert(answer_b == 85062)
+    [print(row) for row in answer_b]
+
+    solution: Map = [
+        [1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1,
+            0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+            1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1,
+            0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1,
+            0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1,
+            0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0],
+    ]
+    assert(answer_b == solution)
     # submit(answer_b, part="b")
 
 
