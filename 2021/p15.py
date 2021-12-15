32f026dcee6c965013cf2b53f7ef4e196b0ff036
Jakob Wagner <jakob.wagner@methodpark.de>
2021-12-15 21:16:01 +0100

    works

--- 2021/p15.py
+++ 2021/p15.py
@@ -3,8 +3,9 @@ from typing import List, Tuple, Dict, Set
 from aocd import lines, submit
 # from copy import deepcopy
 # from functools import reduce
-from itertools import accumulate
+from itertools import accumulate, chain
 from operator import add
+from collections import defaultdict
 
 
 Map = List[List[int]]
@@ -24,34 +25,70 @@ def point(index: int, map: Map) -> Point:
 
 
 def combine_tuples(func, iterable1, iterable2):
-    return list(map(func, iterable1, iterable2))
+    return tuple(map(func, iterable1, iterable2))
 
 
 neighbour_offsets: List[Point] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 def dijkstra(map: Map) -> List[int]:
-    predecessors: List[int] = [-1] * (len(map) * len(map[0]))
-    distances: List[int] = [0] + [-1] * (len(map) * len(map[0]) - 1)
-    unvisited: Set[int] = set(range(len(predecessors)))
-    while unvisited:
-        def min_dist(x, y): return x if distances[x] <= distances[y] else y
-        closest = next(accumulate(unvisited, min_dist))
-        unvisited.remove(closest)
-        for neighbour_offset in neighbour_offsets:
-            neighbour = combine_tuples(
-                add, point(closest, map), neighbour_offset)
-            if neighbour[0] < 0 or neighbour[0] == len(map) \
-                    or neighbour[1] < 0 or neighbour[1] == len(map[0]):
-                continue
-            neighbour_index = index(neighbour, map)
-            # print(neighbour)
-            new_distance = distances[closest] + \
-                map[neighbour[0]][neighbour[1]]
-            if distances[neighbour_index] < 0 or distances[neighbour_index] > new_distance:
-                distances[neighbour_index] = new_distance
-                predecessors[neighbour_index] = closest
+    predecessors: List[int] = defaultdict(lambda: -1)
+    distances: List[int] = defaultdict(lambda: -1)
+    distances[0] = 0
+    change = True
+    while change:
+        change = False
+        for y in range(len(map)):
+            for x in range(len(map[0])):
+                closest = index((y, x), map)
+                for neighbour_offset in neighbour_offsets:
+                    neighbour = combine_tuples(
+                        add, (y, x), neighbour_offset)
+                    if neighbour[0] < 0 or neighbour[0] == len(map) \
+                            or neighbour[1] < 0 or neighbour[1] == len(map[0]):
+                        continue
+                    neighbour_index = index(neighbour, map)
+                    new_distance = distances[closest] + \
+                        map[neighbour[0]][neighbour[1]]
+                    if distances[neighbour_index] < 0 or distances[neighbour_index] > new_distance:
+                        distances[neighbour_index] = new_distance
+                        predecessors[neighbour_index] = closest
+                        change = True
+                    assert(distances[neighbour_index] >= 0)
+    # for y in range(len(map)):
+    #     for x in range(len(map[0])):
+    #         print(f" {distances[index((y,x), map)]} ", end="")
+    #     print("")
     return predecessors
+
+# def dijkstra(map: Map) -> List[int]:
+#     predecessors: List[int] = [-1] * (len(map) * len(map[0]))
+#     distances: List[int] = [0] + [-1] * (len(map) * len(map[0]) - 1)
+#     unvisited: Set[int] = set(range(len(map) * len(map[0])))
+#     i = 0
+#     while unvisited:
+#         i += 1
+#         def min_dist(
+#             x, y): return x if distances[x] < distances[y] or distances[y] < 0 else y
+#         closest = next(accumulate(unvisited, min_dist))
+#         assert(distances[closest] >= 0)
+#         unvisited.remove(closest)
+#         for neighbour_offset in neighbour_offsets:
+#             neighbour = combine_tuples(
+#                 add, point(closest, map), neighbour_offset)
+#             if neighbour[0] < 0 or neighbour[0] == len(map) \
+#                     or neighbour[1] < 0 or neighbour[1] == len(map[0]):
+#                 continue
+#             neighbour_index = index(neighbour, map)
+#             # print(neighbour)
+#             new_distance = distances[closest] + \
+#                 map[neighbour[0]][neighbour[1]]
+#             if distances[neighbour_index] < 0 or distances[neighbour_index] > new_distance:
+#                 distances[neighbour_index] = new_distance
+#                 predecessors[neighbour_index] = closest
+#             assert(distances[neighbour_index] >= 0)
+#     assert(i == len(predecessors))
+#     return predecessors
 
 
 def part1(map: Map) -> int:
@@ -60,21 +97,23 @@ def part1(map: Map) -> int:
     cost = 0
     path = []
     while curr != 0:
+        assert(curr > 0)
         curr_point = point(curr, map)
         path += [curr_point]
         # print(f"{curr_point}, cost {map[curr_point[0]][curr_point[1]]}")
         cost += map[curr_point[0]][curr_point[1]]
         curr = predecessors[curr]
-    for y in range(len(map)):
-        for x in range(len(map[0])):
-            if (y, x) in path:
-                print(f"[{map[y][x]}]", end="")
-            else:
-                print(f" {map[y][x]} ", end="")
-        print("")
+    # for y in range(len(map)):
+    #     for x in range(len(map[0])):
+    #         if (y, x) in path:
+    #             print(f"[{map[y][x]}]", end="")
+    #         else:
+    #             print(f" {map[y][x]} ", end="")
+    #     print("")
+
     # [print(row) for row in map]
 
-    print(cost)
+    # print(cost)
     return cost
 
 
@@ -87,17 +126,13 @@ def increase_risk(val: int, amount: int) -> int:
 
 
 def part2(map: Map) -> int:
-    def bla(row):
-        return row \
-            + elementwise(lambda x: increase_risk(x, 1), row) \
-            + elementwise(lambda x: increase_risk(x, 2), row) \
-            + elementwise(lambda x: increase_risk(x, 3), row) \
-            + elementwise(lambda x: increase_risk(x, 4), row)
-    map = [bla(row) for row in map]
-    map += [elementwise(lambda x: increase_risk(x, 1), row) for row in map] \
-        + [elementwise(lambda x: increase_risk(x, 2), row) for row in map] \
-        + [elementwise(lambda x: increase_risk(x, 3), row) for row in map] \
-        + [elementwise(lambda x: increase_risk(x, 4), row) for row in map]
+    def expand(row):
+        return list(chain(row, *[elementwise(lambda x: increase_risk(x, i), row)
+                                 for i in range(1, 5)]))
+    map = [expand(row) for row in map]
+    map = list(chain(map, *[[elementwise(lambda x: increase_risk(
+        x, i), row) for row in map] for i in range(1, 5)]))
+    [print(row) for row in map]
     return part1(map)
 
 
@@ -116,7 +151,7 @@ def main():
     ]
 
     example_map = parse(example)
-    assert(part1(example_map, ) == 40)
+    assert(part1(example_map) == 40)
 
     map = parse(lines)
     answer_a = part1(map)
@@ -125,11 +160,11 @@ def main():
     # submit(answer_a, part="a")
 
     assert(part2(example_map) == 315)
-    # answer_b = part2(map)
-    # print(f"b {answer_b}")
+    answer_b = part2(map)
+    print(f"b {answer_b}")
 
-    # assert(answer_b < 2932)
-    # submit(answer_b, part="b")
+    assert(answer_b < 2932)
+    submit(answer_b, part="b")
 
 
 if __name__ == "__main__":
